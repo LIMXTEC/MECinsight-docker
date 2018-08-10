c7829ab3932466b88d66fdf9f143cacff9137982
David <37987466+dalijolijo@users.noreply.github.com>
2018-08-10 14:33:09 +0200

    Create script to downloading additional nodes file and add nodes after start

--- start.sh
+++ start.sh
@@ -9,5 +9,12 @@ if [ ! -d /home/megacoin/bitcore-livenet/bin/mynode/data/data/blocks ] && [ "$(c
         rm ${BOOTSTRAP}; \
 fi
 
+# Create script to downloading additional nodes file and add nodes after start with 'docker exec mec-insight-docker addnodes.sh'
+echo "#!/bin/bash" > /usr/local/bin/addnodes.sh
+echo "wget https://raw.githubusercontent.com/dalijolijo/MECinsight-docker/master/addnodes.conf -O /home/megacoin/bitcore-livenet/bin/mynode/data/addnodes.conf" >> /usr/local/bin/addnodes.sh
+echo "cat /home/megacoin/bitcore-livenet/bin/mynode/data/addnodes.conf >> /home/megacoin/bitcore-livenet/bin/mynode/data/megacoin.conf" >> /usr/local/bin/addnodes.sh
+echo "supervisorctl restart megacoind" >> /usr/local/bin/addnodes.sh
+chmod 755 /usr/local/bin/addnodes.sh
+
 # Starting Supervisor Service
 exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
